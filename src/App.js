beb4f00143c02167a34c5751192c0c02c177449a
akmahamud <am3153636@gmail.com>
2022-03-16 09:35:33 +0600

    react core concept

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+     <Counter></Counter>
+   
     </div>
   );
+}
+
+function Counter(){
+  
+ let [count, setCount] = useState(50);
+  const Increment = () => setCount(count+1);
+  const decrement = () => setCount(count-1);
+  
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick = {Increment}>Increment</button>
+      <button onClick = {decrement}>decrement</button>
+      
+      
+    </div>
+  )
 }
 
 export default App;
